1c019f47b41766b0cdd9e2a0dae2279f083846c8
magatsu-no-imouto <148965400+magatsu-no-imouto@users.noreply.github.com>
2024-03-07 11:06:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,11 +26,7 @@ connecto();
 const exp = require('express');
 const AWS = require("aws-sdk");
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
+const s3 = new AWS.S3();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
