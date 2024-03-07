ecb83f4fcea3420583c8d469b6640a6b22074a89
magatsu-no-imouto <148965400+magatsu-no-imouto@users.noreply.github.com>
2024-03-07 11:29:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ train.get('/room-data', async (req, res) => {
       if (room.roomImg.startsWith('https://')) {
         try {
           const s3File = await s3.getObject({
-            Bucket: process.env.BUCKET_NAME,
+            Bucket: cyclic-calm-pink-glasses-ap-southeast-1,
             Key: room.roomImg,
           }).promise();
           
@@ -581,7 +581,7 @@ train.post('/insertRoom',upload.single('roomImg'),async(req,res)=>{
           return res.status(500).json({error: 'Database is not ready.'});
       }
       const params = {
-        Bucket: process.env.BUCKET_NAME,
+        Bucket: cyclic-calm-pink-glasses-ap-southeast-1,
         Key: `media/${req.file.originalname}`,
         Body: req.file.buffer,
         ContentType: req.file.mimetype,
@@ -591,7 +591,7 @@ train.post('/insertRoom',upload.single('roomImg'),async(req,res)=>{
 
       
       await rider.collection('rooms').insertOne({
-        roomName,roomImg:`https://${process.env.BUCKET_NAME}.s3.amazonaws.com/media/${req.file.originalname}`,roomDesc,roomSize,roomCost,roomReservd,roomLeft
+        roomName,roomImg:`https://cyclic-calm-pink-glasses-ap-southeast-1.s3.amazonaws.com/media/${req.file.originalname}`,roomDesc,roomSize,roomCost,roomReservd,roomLeft
       });
       console.log('Record Inserted!');
       res.status(201).json({message:'Record Inserted!'});
